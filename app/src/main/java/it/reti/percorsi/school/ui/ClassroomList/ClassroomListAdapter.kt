cbe4238adbc657335df853b369060513990e5f36
Davide Oggioni <33121159+daviogg@users.noreply.github.com>
2020-01-19 22:23:20 +0100

    Modified Lista adapter and fragment

---
+++ app/src/main/java/it/reti/percorsi/school/ui/ClassroomList/ClassroomListAdapter.kt
@@ -0,0 +1,34 @@
+package it.reti.percorsi.school.ui.ClassroomList
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import it.reti.percorsi.school.R
+import it.reti.percorsi.school.db.entities.Classroom
+
+class ClassroomListAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<ClassroomListAdapter.ClassroomViewholder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var classrooms = emptyList<Classroom>() // Cached copy of words
+
+    inner class ClassroomViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemView: TextView = itemView.findViewById(R.id.class_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClassroomViewholder {
+        val itemView = inflater.inflate(R.layout.list_classroom_fragment, parent, false)
+        return ClassroomViewholder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ClassroomViewholder, position: Int) {
+        val current = classrooms[position]
+        holder.wordItemView.text = current.name
+    }
+
+    override fun getItemCount() = classrooms.size
+}
